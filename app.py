49b39c8c2f9e671c22848974c4ffaef9c93a8899
dhiru-c <dhirajchaudhari481@gmail.com>
2022-06-26 01:19:45 +0530

    file updated

--- app.py
+++ app.py
@@ -1,57 +1,90 @@
-import streamlit as st
-from keras.models import load_model
-import cv2
+# Importing Libraries
 import numpy as np
-from time import sleep
-from keras.utils import img_to_array
+import cv2
+import streamlit as st
+#from tensorflow import keras
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer,video_processor_factory
+
+# load model
+emotion_dict = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
 
 classifier = load_model('mobilenet.h5')
 
-class_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+# load weights into new model
+classifier.load_weights("vgg19.h5")
 
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-st.title("Face Emotion Detection")
-
-FRAME_WINDOW = st.image([])
-
-
-def face_detector(img):
-    # Convert image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-    if faces == ():
-        return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-
-    try:
-        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-    except:
-        return (x, w, y, h), np.zeros((48, 48), np.uint8), img
-    return (x, w, y, h), roi_gray, img
+# load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
 
 
-cap = cv2.VideoCapture(0)
+class VideoTransformer(video_processor_factory):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-while True:
+        # image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    ret, frame = cap.read()
-    rect, face, image = face_detector(frame)
-    if np.sum([face]) != 0.0:
-        roi = face.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = np.expand_dims(roi, axis=0)
+        return img
 
-        # make a prediction on the ROI, then lookup the class
-        preds = classifier.predict(roi)[0]
-        label = class_labels[preds.argmax()]
-        label_position = (rect[0] + int((rect[1] / 2)), rect[2] + 25)
-        cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def main():
+    # Face Analysis Application #
+    st.title("Real Time Face Emotion Detection Application")
+    activiteis = ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activiteis)
+
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+        st.write("""
+                 The application has two functionalities.
+                 1. Real time face detection using web cam feed.
+                 2. Real time face emotion recognization.
+                 """)
+    elif choice == "Webcam Face Detection":
+        st.header("Webcam Live Feed")
+        st.write("Click on start to use webcam and detect your face emotion")
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    elif choice == "About":
+        st.subheader("About this app")
+        html_temp_about1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                    <h4 style="color:white;text-align:center;">
+                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
+                                    </div>
+                                    </br>"""
+        st.markdown(html_temp_about1, unsafe_allow_html=True)
+
+
     else:
-        cv2.putText(image, "No Face Found", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        pass
 
-    # cv2.imshow('All', image)
-    FRAME_WINDOW.image(image)
+
+if __name__ == "__main__":
+    main()
